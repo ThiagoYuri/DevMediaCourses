01fe43aa56001b8081ccba11e4a86bf21f2e2fcd
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-11-13 23:20:32 -0300

    completed course

--- Web/curso-javascript/js/index.js
+++ Web/curso-javascript/js/index.js
@@ -1,6 +1,7 @@
 //console.log('arquivo carregado') //commited
 
-/*/var nome = 'Thiago'
+/*/
+var nome = 'Thiago'
 //Create variable
 var idade = 25
 var peso = 85.5
@@ -13,6 +14,8 @@ console.log(peso,typeof peso)
 console.log(maiorDeIdade,typeof maiorDeIdade)
 /*/
 
+/*/
+//create array
 var nomes = [
     'joão',
     'maria',
@@ -21,6 +24,7 @@ var nomes = [
 ]
 console.log(nomes[1])
 
+//create object
 var pessoa = {
     nome: 'bruno',
     amigos: [
@@ -31,4 +35,153 @@ var pessoa = {
 }
 
 console.log(pessoa.amigos[2])
+/*/
 
+/*/
+var peso = 80
+var altura = 1.83
+var imc = peso / (altura*altura)
+
+imc += 10
+imc -= 10
+imc ++
+//imc --
+
+console.log(imc)
+/*/
+
+/*/
+var comprarRefrigerante = false
+var comprarCerveja = false
+var comprarSucoNatural = false
+
+var convidado = {
+    nome: 'pedro',
+    idade: 17,
+    bebeCerveja:true,
+    bebeRefrigerante:false,
+}
+
+if(convidado.bebeRefrigerante){
+    comprarRefrigerante=true
+}else if(convidado.bebeCerveja && convidado.idade >= 18){
+    comprarCerveja=true
+}else{
+    comprarSucoNatural = true
+}
+
+
+console.log(comprarRefrigerante)
+console.log(comprarCerveja)
+console.log(comprarSucoNatural)
+/*/
+
+/*/
+
+var comprarRefrigerante = false
+var comprarCerveja = false
+var comprarSucoNatural = false
+
+var convidados = [
+    {
+        nome: 'pedro',
+        idade: 17,
+        bebeCerveja:true,
+        bebeRefrigerante:true,
+    },
+    {
+        nome: 'Joao',
+        idade: 23,
+        bebeCerveja:false,
+        bebeRefrigerante:true,
+    },
+    {
+        nome: 'Kilson',
+        idade: 34,
+        bebeCerveja:true,
+        bebeRefrigerante:true,
+    },
+    {
+        nome: 'Felipe',
+        idade: 20,
+        bebeCerveja:true,
+        bebeRefrigerante:false,
+    },
+]
+
+var count = 0
+console.log(convidados.count)
+for(var i= 0; i< convidados.length; i++){
+
+       
+    if(convidados[i].bebeRefrigerante){
+        comprarRefrigerante=true
+    }else if(convidados[i].bebeCerveja && convidados[i].idade >= 18){
+        comprarCerveja=true
+    }else{
+        comprarSucoNatural = true
+    }
+    console.log(convidados[i].nome, comprarRefrigerante, comprarCerveja , comprarSucoNatural)
+  
+}
+/*/
+
+
+/*/
+IMC(80.70,1.75)
+IMC(90.70,1.75)
+
+function IMC(peso,altura){
+    var imc = peso / (altura*altura)
+
+    imc += 10
+    imc -= 10
+    imc ++
+    //imc --
+
+    console.log(imc)
+    return imc
+}
+/*/
+
+var pessoas = [
+    {
+        nome:'Thiago',
+        altura: 1.70,
+        peso:80
+    },
+    {
+        nome:'Luiz',
+        altura: 1.90,
+        peso:20
+    },
+    {
+        nome:'Rodrigo',
+        altura: 2,
+        peso:90
+    },
+    {
+        nome:'Jose',
+        altura: 1.40,
+        peso:65
+    }
+]
+
+function IMC(peso,altura){
+    var imc = peso / (altura*altura)
+
+    imc += 10
+    imc -= 10
+    imc ++
+    //imc --
+
+    return imc
+}
+
+
+for(var pessoa of pessoas){
+  var resultado =IMC(pessoa.peso,pessoa.altura)
+
+  console.log(pessoa.nome , resultado)
+
+}
