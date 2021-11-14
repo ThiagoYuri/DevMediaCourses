1b23bbdda1280a947cbc35290909537ab37b6f32
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-11-13 22:38:49 -0300

    Array and object

--- Web/curso-javascript/js/index.js
+++ Web/curso-javascript/js/index.js
@@ -1,12 +1,34 @@
 //console.log('arquivo carregado') //commited
 
-var nome = 'Thiago'
+/*/var nome = 'Thiago'
+//Create variable
 var idade = 25
 var peso = 85.5
 var maiorDeIdade = true
 maiorDeIdade = 'false'
-
+//see value of variable with the type
 console.log(nome,typeof nome)
 console.log(idade,typeof idade)
 console.log(peso,typeof peso)
 console.log(maiorDeIdade,typeof maiorDeIdade)
+/*/
+
+var nomes = [
+    'joão',
+    'maria',
+    'pedro',
+    //true // is validad
+]
+console.log(nomes[1])
+
+var pessoa = {
+    nome: 'bruno',
+    amigos: [
+        'joão',
+        'maria',
+        'pedro'
+    ]
+}
+
+console.log(pessoa.amigos[2])
+
