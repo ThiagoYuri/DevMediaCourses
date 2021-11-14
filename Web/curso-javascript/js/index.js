c6b399e7283ce1b51caeb9251e2041d96464fa16
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-11-13 22:27:49 -0300

    Create variable in javascript

--- Web/curso-javascript/js/index.js
+++ Web/curso-javascript/js/index.js
@@ -1,1 +1,12 @@
-console.log('arquivo carregado')
+//console.log('arquivo carregado') //commited
+
+var nome = 'Thiago'
+var idade = 25
+var peso = 85.5
+var maiorDeIdade = true
+maiorDeIdade = 'false'
+
+console.log(nome,typeof nome)
+console.log(idade,typeof idade)
+console.log(peso,typeof peso)
+console.log(maiorDeIdade,typeof maiorDeIdade)
